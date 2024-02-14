ed13c8351573f3f920fa7cacdb539025574309d9
Astatine <87228325+cpu-astatine@users.noreply.github.com>
2024-02-14 01:13:30 -0500

    Create python_install.sh

---
+++ python_install.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install python -y
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y
+bash "Python kurulumu tamamlandi, paketler kurulup güncellendi."
