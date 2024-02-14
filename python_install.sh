25db743a8374e8789bcccfe392274b6698734116
Astatine <87228325+cpu-astatine@users.noreply.github.com>
2024-02-14 03:44:20 -0500

    Update python_install.sh

--- python_install.sh
+++ python_install.sh
@@ -3,4 +3,4 @@ sudo apt-get upgrade -y
 sudo apt-get install python -y
 sudo apt-get install python3 -y
 sudo apt-get install python3-pip -y
-bash "Python kurulumu tamamlandi, paketler kurulup güncellendi."
+echo "Python kurulumu tamamlandi, paketler kurulup güncellendi."
